1633f67c117a6153470d2e71656b5b3d1ebd8eff
J4nk37 <geometrydown40@gmail.com>
2025-10-20 09:10:19 +0200

    Add comments for code clarity in contadorPalabras
    Added comments to clarify the code's functionality.

--- ContadorPalabras/src/main/java/org/example/contadorPalabras.java
+++ ContadorPalabras/src/main/java/org/example/contadorPalabras.java
@@ -5,10 +5,14 @@ import java.io.*;
 public class contadorPalabras {
     public static void main(String[] args) {
         int total = 0;
+        // Creo un reader dentro del try que reciba los datos de la consola
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String linea;
+            // Creo un bucle que lea los datos de la consola
             while ((linea = br.readLine()) != null) {
+                // Utilizo la funcion trim y split para procesar las lineas 1 a 1
                 String[] palabras = linea.trim().split("\\s+");
+                // Creo un bucle para que vaya contando las palabras al leerla si estas existen
                 for (String p : palabras) {
                     if (!p.isEmpty()) total++;
                 }
@@ -19,4 +23,5 @@ public class contadorPalabras {
         }
     }
 }
+
 
