7765e0d7e69eb87aa07f4f2bbc2473ef4fb64fc0
J4nk37 <geometrydown40@gmail.com>
2025-10-20 09:05:59 +0200

    Add comments for code clarity in filtrarLineas.java
    Added comments to clarify the code functionality.

--- FiltrarLineas/src/main/java/org/example/filtrarLineas.java
+++ FiltrarLineas/src/main/java/org/example/filtrarLineas.java
@@ -4,9 +4,12 @@ import java.io.*;
 
 public class filtrarLineas {
     public static void main( String[] args ){
+        // Creo un reader que utiliza los datos que la consola ha mostrado
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));){
             String linea;
+            // Creo un bucle que lea los datos mostrados en consola
             while((linea = br.readLine())!=null) {
+                // Filtro por las lineas mayores a 20 palabras
                 if (linea.length() > 20) {
                     System.out.println(linea);
                 }
@@ -16,4 +19,5 @@ public class filtrarLineas {
         }
     }
 }
+
 
