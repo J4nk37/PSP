24cc430d22ee542075e99a361954097ebcec1520
J4nk37 <geometrydown40@gmail.com>
2025-10-20 09:02:17 +0200

    Update lectorTexto.java

--- LectorTexto/src/main/java/org/example/lectorTexto.java
+++ LectorTexto/src/main/java/org/example/lectorTexto.java
@@ -4,11 +4,14 @@ import java.io.*;
 
 public class lectorTexto {
     public static void main( String[] args ) {
+        // Creo la variable que utilizara el entrada.txt como Stream
         InputStream isr = lectorTexto.class.getClassLoader().getResourceAsStream("entrada.txt");
 
         try {
+            // Creo el reader y hago que utilize la variable creada anteriormente
             BufferedReader br = new BufferedReader(new InputStreamReader(isr));
             String linea;
+            // Uso el bucle while para leer entrada.txt y que imprima las lineas que vaya encontrando
             while((linea = br.readLine()) != null){
                 System.out.println(linea);
             }
@@ -20,4 +23,3 @@ public class lectorTexto {
     }
 
 }
-
